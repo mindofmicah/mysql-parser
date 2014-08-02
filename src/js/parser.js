878bbb9ae2c34815488e42c468a1a9bc8be1901a
micah eschbacher <micah@whamdonk.com>
2014-08-02 12:10:09 -0400

    Added parsing for column information

--- src/js/parser.js
+++ src/js/parser.js
@@ -1,7 +1,41 @@
 var Parser = function () {};
 
-Parser.prototype.parse = function () {
-    return new MySQL;
+Parser.prototype.buildColumns = function (input) {
+    if (input.indexOf('*') != -1) {
+        return ['*']
+    }
+
+    var match = input.match(/select (.+) from/i);
+
+    var clauses = match[1].split(/, ?/);
+
+    var ret = [];
+    var current_item = '';
+    var open_parens = 0;
+    for (var i=0; i < clauses.length; i++) {
+        if (clauses[i].indexOf('(') != -1) {
+            open_parens++;
+            current_item+=clauses[i];
+        } else if (clauses[i].indexOf(')') != -1) {
+            open_parens--;
+            ret.push(current_item +', ' + clauses[i]);
+            current_item = '';
+        } else {
+            ret.push(clauses[i]);
+        }
+    }
+    return ret;
+};
+Parser.prototype.parse = function (input) {
+    var mysql = new MySQL;
+
+    if ((input || '').length === 0) {
+        return mysql;
+    }
+
+    mysql.table = 'table_1';
+    mysql.columns = this.buildColumns(input||'');
+    return mysql;
 };
 
 
--- tests/parserSpec.js
+++ tests/parserSpec.js
@@ -11,6 +11,23 @@ describe('Parser Class', function () {
             var parser = new Parser;
             expect(parser.parse() instanceof MySQL).toBe(true);
         });
+
+        it('should parse a basic select star from statement', function () {
+            var parser = new Parser;
+            var mysql = parser.parse('SELECT * FROM table_1');
+            expect(mysql.columns).toEqual(['*']);
+            expect(mysql.table).toBe('table_1');
+        });
+        it('should be able to have multiple columns', function () {
+            var parser = new Parser;
+            var mysql = parser.parse('SELECT col1, col2 FROM table_1');
+            expect(mysql.columns).toEqual(['col1', 'col2']);
+        });
+        it('should handle complex select statements', function () {
+            var parser = new Parser;
+            var mysql = parser.parse('SELECT IFNULL(col1, "apple") AS col1, col2 FROM table');
+            expect(mysql.columns).toEqual(['IFNULL(col1, "apple") AS col1', 'col2']);
+        });
     });
 });
 
